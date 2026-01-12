0808cc5bd1215194090f9771e9c3e7de4245bb5d
BennyTMT <35400831+BennyTMT@users.noreply.github.com>
2026-01-12 01:03:27 +0000

    Deploying to gh-pages from @ BennyTMT/mingtiant@8bd904251918a06cd1175ab80f9f6a0a867ce840 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,21 +16,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/mingtiant/people/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/mingtiant/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/mingtiant/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
